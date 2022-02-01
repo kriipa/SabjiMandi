ab5a8a86589f2a07f35d56d7120e1672bd15f19b
kriipa <kripathapa2002@gmail.com>
2022-02-01 22:37:53 +0545

    new commit

--- Sabji_Mandi/Mandi/tests.py
+++ Sabji_Mandi/Mandi/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from audioop import reverse
+from http import client
+from pydoc import resolve
+from django.test import TestCase, SimpleTestCase
+from django.urls import path
 
 # Create your tests here.
+
+class TestUrls(SimpleTestCase):
+    def test_case_index_url(self):
+        url = reverse('register')
+        self.assertEquals(resolve(url).func,)
+
+
+
+class TestViews(TestCase):
+    def test_case_register_view(self):
+        client=client()
+        print(client)
